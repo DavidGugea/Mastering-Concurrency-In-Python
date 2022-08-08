5c3982aa35a41c44ac33def6ef44eb60c4a90d0e
David Gugea <davidgugea@yahoo.com>
2022-08-08 16:06:53 +0200

    Finished Chapter 9

---
+++ Chapter9/example2.py
@@ -0,0 +1,24 @@
+from math import sqrt
+import asyncio
+
+
+async def is_prime(x):
+    print("Processing {0}".format(x))
+
+    if x < 2:
+        print("{0} is not a prime number".format(x))
+    elif x == 2:
+        print("{0} is a prime number".format(x))
+    elif x % 2 == 0:
+        print("{0} is not a prime number".format(x))
+    else:
+        limit = int(sqrt(x)) + 1
+
+        for i in range(3, limit, 2):
+            if x % i == 0:
+                print("{0} is not a prime number.".format(x))
+                return
+            elif i % 100000 == 1:
+                await asyncio.sleep(0)
+
+        print("{0} is a prime number".format(x))
